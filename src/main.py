492662ec63a6ca621ae31fa2a3b5e6478aed0627
jackesfonseca <jackesdafonseca@gmail.com>
2023-05-13 16:55:48 -0300

    changing colot

--- src/main.py
+++ src/main.py
@@ -14,27 +14,27 @@ if __name__=='__main__':
     # myMaze.CreateMaze(loadMaze='dijkMaze.csv')
 
     # Define em qual região vai receber um valor maior que 1
-    h1=agent(myMaze,1,randint(2,9),color=COLOR.red)
-    h2=agent(myMaze,1,randint(2,9),color=COLOR.red)
-    h3=agent(myMaze,1,randint(2,9),color=COLOR.red)
-    h4=agent(myMaze,2,randint(1,9),color=COLOR.red)
-    h5=agent(myMaze,2,randint(1,9),color=COLOR.red)
-    h6=agent(myMaze,2,randint(1,9),color=COLOR.red)
-    h7=agent(myMaze,3,randint(1,9),color=COLOR.red)
-    h8=agent(myMaze,3,randint(1,9),color=COLOR.red)
-    h9=agent(myMaze,3,randint(1,9),color=COLOR.red)
-    h10=agent(myMaze,4,randint(1,9),color=COLOR.red)
-    h11=agent(myMaze,4,randint(1,9),color=COLOR.red)
-    h12=agent(myMaze,4,randint(1,9),color=COLOR.red)
-    h13=agent(myMaze,5,randint(1,9),color=COLOR.red)
-    h14=agent(myMaze,5,randint(1,9),color=COLOR.red)
-    h15=agent(myMaze,5,randint(1,9),color=COLOR.red)
-    h16=agent(myMaze,6,randint(1,9),color=COLOR.red)
-    h17=agent(myMaze,6,randint(1,9),color=COLOR.red)
-    h18=agent(myMaze,6,randint(1,9),color=COLOR.red)
-    h19=agent(myMaze,7,randint(1,8),color=COLOR.red)
-    h20=agent(myMaze,7,randint(1,8),color=COLOR.red)
-    h21=agent(myMaze,7,randint(1,8),color=COLOR.red)
+    h1=agent(myMaze,1,randint(2,9),color=COLOR.green)
+    h2=agent(myMaze,1,randint(2,9),color=COLOR.green)
+    h3=agent(myMaze,1,randint(2,9),color=COLOR.green)
+    h4=agent(myMaze,2,randint(1,9),color=COLOR.green)
+    h5=agent(myMaze,2,randint(1,9),color=COLOR.green)
+    h6=agent(myMaze,2,randint(1,9),color=COLOR.green)
+    h7=agent(myMaze,3,randint(1,9),color=COLOR.green)
+    h8=agent(myMaze,3,randint(1,9),color=COLOR.green)
+    h9=agent(myMaze,3,randint(1,9),color=COLOR.green)
+    h10=agent(myMaze,4,randint(1,9),color=COLOR.green)
+    h11=agent(myMaze,4,randint(1,9),color=COLOR.green)
+    h12=agent(myMaze,4,randint(1,9),color=COLOR.green)
+    h13=agent(myMaze,5,randint(1,9),color=COLOR.green)
+    h14=agent(myMaze,5,randint(1,9),color=COLOR.green)
+    h15=agent(myMaze,5,randint(1,9),color=COLOR.green)
+    h16=agent(myMaze,6,randint(1,9),color=COLOR.green)
+    h17=agent(myMaze,6,randint(1,9),color=COLOR.green)
+    h18=agent(myMaze,6,randint(1,9),color=COLOR.green)
+    h19=agent(myMaze,7,randint(1,8),color=COLOR.green)
+    h20=agent(myMaze,7,randint(1,8),color=COLOR.green)
+    h21=agent(myMaze,7,randint(1,8),color=COLOR.green)
 
     # Define a o valor que uma região recebeu
     h1.cost=randint(10,25)
@@ -59,14 +59,12 @@ if __name__=='__main__':
     h20.cost=randint(10,25)
     h21.cost=randint(10,25)
 
-
-
     # Define onde o labirinto inicia e passa as regiões que possui distância maior que 1
     path,c = dijkstra(myMaze,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,start=(1,1))
     textLabel(myMaze,'Total Cost',c)
 
     # Cria a função que mostra o menor caminho sendo construído
-    a = agent(myMaze,1,1,color=COLOR.cyan,filled=True,footprints=True)
+    a = agent(myMaze,1,1,color=COLOR.red,filled=True,footprints=True)
     myMaze.tracePath({a:path})
 
     # Inicia a aplicação
