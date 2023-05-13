9b1fd239e0d69e2f2e6bd850c73b82a0f6fb1240
jackesfonseca <jackesdafonseca@gmail.com>
2023-05-13 16:23:01 -0300

    updating dijkstra file

--- src/main.py
+++ src/main.py
@@ -1,54 +1,6 @@
 from random import randint
 from pyamaze.pyamaze import maze,agent,COLOR,textLabel
-
-# Inicia o algoritmo de dijkstra com busca em camadas para percorrer o labirinto
-def dijkstra(m,*h,start=None):
-    if start is None:
-        start=(m.rows,m.cols)
-
-    hurdles=[(i.position,i.cost) for i in h]
-
-    unvisited={n:float('inf') for n in m.grid}
-    unvisited[start]=0
-    visited={}
-    revPath={}
-    while unvisited:
-        currCell=min(unvisited,key=unvisited.get)
-        visited[currCell]=unvisited[currCell]
-        if currCell==m._goal:
-            break
-        for d in 'EWNS':
-            if m.maze_map[currCell][d]==True:
-                if d=='E':
-                    childCell=(currCell[0],currCell[1]+1)
-                elif d=='W':
-                    childCell=(currCell[0],currCell[1]-1)
-                elif d=='S':
-                    childCell=(currCell[0]+1,currCell[1])
-                elif d=='N':
-                    childCell=(currCell[0]-1,currCell[1])
-                if childCell in visited:
-                    continue
-                tempDist= unvisited[currCell]+1
-                for hurdle in hurdles:
-                    if hurdle[0]==currCell:
-                        tempDist+=hurdle[1]
-
-                if tempDist < unvisited[childCell]:
-                    unvisited[childCell]=tempDist
-                    revPath[childCell]=currCell
-        unvisited.pop(currCell)
-    
-    fwdPath={}
-    cell=m._goal
-    while cell!=start:
-        fwdPath[revPath[cell]]=cell
-        cell=revPath[cell]
-    
-    return fwdPath,visited[m._goal]
-            
-
-
+from dijsktra import dijkstra
 
 if __name__=='__main__':
     #  Define o tamanho do labirinto
@@ -56,6 +8,7 @@ if __name__=='__main__':
 
     # Define o ponto de chegada do labirinto e o quão juntas as paredes vão ser
     myMaze.CreateMaze(7,9,loopPercent=200)
+    #print(myMaze.maze_map)
 
     # Cria um labirinto a partir de um arquivo .csv
     # myMaze.CreateMaze(loadMaze='dijkMaze.csv')
@@ -109,11 +62,11 @@ if __name__=='__main__':
 
 
     # Define onde o labirinto inicia e passa as regiões que possui distância maior que 1
-    path,c=dijkstra(myMaze,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,start=(1,1))
+    path,c = dijkstra(myMaze,h1,h2,h3,h4,h5,h6,h7,h8,h9,h10,h11,h12,h13,h14,h15,h16,h17,h18,h19,h20,start=(1,1))
     textLabel(myMaze,'Total Cost',c)
 
     # Cria a função que mostra o menor caminho sendo construído
-    a=agent(myMaze,1,1,color=COLOR.cyan,filled=True,footprints=True)
+    a = agent(myMaze,1,1,color=COLOR.cyan,filled=True,footprints=True)
     myMaze.tracePath({a:path})
 
     # Inicia a aplicação
